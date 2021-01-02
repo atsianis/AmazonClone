948ab9a657821a2c240874ba2cf49bbe74d91eb5
Thanos Tsianis <nektariosts@gmail.com>
2021-01-03 01:42:46 +0200

    add hardcoded id to product

--- src/Home.js
+++ src/Home.js
@@ -6,9 +6,10 @@ function Home() {
     return (
         <div className = 'home'>
             <div className="home__container">
-                <img className = "home__image" src = "https://cdn.technadu.com/wp-content/uploads/2018/07/Amazon-Prime-Day-2018-Featured-Banner.jpg" alt="Amazon Banner"/>
+                <img className = "home__image" src = "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2021/NYNY/Fuji_TallHero_NYNY_en_US_1x._CB412256579_.jpg" alt='Banner'/>
                 <div className="home__row">
                     <Product 
+                        id = {234342}
                         title="The lean startup" 
                         price={19.99} 
                         rating={5} 

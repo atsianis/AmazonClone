92e787f8205b5ca1f312a31b9ee45b0c9821bfbe
Thanos Tsianis <nektariosts@gmail.com>
2021-01-03 01:43:43 +0200

    render checkoutProduct in Checkout component

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import './Checkout.css';
+import CheckoutProduct from './CheckoutProduct';
+import Product from './Product';
+import { useStateValue } from './StateProvider';
 import Subtotal from './Subtotal';
 
 function Checkout() {
+
+    const [{basket}, dispatch] = useStateValue();
+
+    console.log(basket);
+
     return (
         <div className='checkout'>
             <div className="checkout__left">
                 <img className='checkout__ad' src="https://www.disruptivestatic.com/wp-content/uploads/2018/10/Screen-Shot-2018-10-29-at-11.50.03-AM-450x96.png" alt='ad'/>
                 <div>
                     <h2 className='checkout__title'>Your Shopping Basket</h2>
+                    {basket.map((product)=>
+                        <CheckoutProduct 
+                            id = {product.id}
+                            title = {product.title}
+                            price = {product.price}
+                            rating = {product.rating}
+                            image = {product.image}
+                        />
+                    )}
                 </div>
             </div>
             <div className="checkout__right">

782476dbc995f0bc229ec5caf2214086c8291b3c
Thanos Tsianis <nektariosts@gmail.com>
2021-01-02 19:53:03 +0200

    create checkout component

---
+++ src/Checkout.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Checkout.css';
+import Subtotal from './Subtotal';
+
+function Checkout() {
+    return (
+        <div className='checkout'>
+            <div className="checkout__left">
+                <img className='checkout__ad' src="https://www.disruptivestatic.com/wp-content/uploads/2018/10/Screen-Shot-2018-10-29-at-11.50.03-AM-450x96.png" alt='ad'/>
+                <div>
+                    <h2 className='checkout__title'>Your Shopping Basket</h2>
+                </div>
+            </div>
+            <div className="checkout__right">
+                <Subtotal/>
+            </div>
+        </div>
+    )
+}
+
+export default Checkout

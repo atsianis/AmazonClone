9bd6eac229f588a3c7bbb0d4b2e62fa91ff30ae2
Thanos Tsianis <nektariosts@gmail.com>
2021-01-10 03:22:15 +0200

    implement autologin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import "./App.css";
+import {useEffect} from 'react';
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+
+  const [{}, dispatch] = useStateValue();
+
+  useEffect( ()=> {
+    auth.onAuthStateChanged( user => {
+      //MAPPING THE CONTEXT API TO THE FIREBASE STATE
+      if (user) {
+        dispatch({
+          type: 'SET_USER',
+          payload: user
+        })
+      }else {
+        dispatch({
+          type: 'SET_USER',
+          payload: null
+        })
+      }
+    })
+  }, [])
+
   return (
     <div className="app">
       <BrowserRouter>
